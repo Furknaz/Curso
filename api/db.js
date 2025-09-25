216c16269d462284a39518528060e95329115d73
Paludetto95 <paludetto12@gmail.com>
2025-09-25 14:16:30 -0300

    correção db

--- api/db.js
+++ api/db.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 // Configuração explícita da conexão com o banco de dados
 const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  port: process.env.MYSQLPORT,
+  database: process.env.MYSQLDATABASE,
   ssl: {
     rejectUnauthorized: false
   },
